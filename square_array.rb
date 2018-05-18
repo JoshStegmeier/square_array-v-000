b264c53142d4eb76fe142c5cab85fb67ba88cd3b
Josh Stegmeier <josh@steg.me>
2018-05-18 05:07:03 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,8 @@
 def square_array(array)
-  # newArray = []
-  # array.each do |number|
-  #   value = number ** 2
-  #   newArray << value
-  # end
-  # return newArray
-
-  array.map { |x| x ** 2 }
+  newArray = []
+  array.each do |number|
+    value = number ** 2
+    newArray << value
+  end
+  return newArray
 end
